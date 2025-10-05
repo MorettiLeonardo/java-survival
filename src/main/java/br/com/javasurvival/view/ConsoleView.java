cfea4ecb4e5d4a6a609eea6575ac21ca16ce622e
leonardo moretti <leonardonabarro2003@gmail.com>
2025-10-05 15:40:12 -0300

    feature: adicionadno view de console

---
+++ src/main/java/br/com/javasurvival/view/ConsoleView.java
@@ -0,0 +1,34 @@
+package br.com.javasurvival.view;
+
+import java.util.Scanner;
+
+public class ConsoleView {
+
+    private Scanner sc = new Scanner(System.in);
+
+    public void exibirMensagem(String msg) {
+        System.out.println(msg);
+    }
+
+    public void exibirMenuPrincipal() {
+        System.out.println("""
+                \n=== MENU PRINCIPAL ===
+                1 - Explorar
+                2 - Descansar
+                3 - Usar item
+                4 - Ver inventário
+                5 - Salvar jogo
+                0 - Sair
+                =====================
+                Escolha uma opção:
+                """);
+    }
+
+    public int lerOpcaoUsuario() {
+        try {
+            return Integer.parseInt(sc.nextLine());
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+}
